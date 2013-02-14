a92414c3905eabc8431ae9c6ba11150168fdc3ca
adrian@git.edia.nl <adrian@git.edia.nl>
2013-02-14 10:07:54 +0200

    Added feature to display only  movies owned by a specific user

--- src/main/java/ro/isdc/auth/repository/MovieRepository.java
+++ src/main/java/ro/isdc/auth/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package ro.isdc.auth.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import ro.isdc.auth.domain.Movie;
@@ -21,4 +23,6 @@ public interface MovieRepository extends PagingAndSortingRepository<Movie, Strin
 	Movie findByDescription(String description);
 
 	List<Movie> findByUserId(String userId);
+
+	Page<Movie> findByUserId(String userId, Pageable pageable);
 }
--- src/main/java/ro/isdc/auth/service/crud/MovieService.java
+++ src/main/java/ro/isdc/auth/service/crud/MovieService.java
@@ -1,13 +1,24 @@
 package ro.isdc.auth.service.crud;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
 
 import ro.isdc.auth.domain.Movie;
 import ro.isdc.auth.helper.EntityHelper;
 import ro.isdc.auth.helper.MovieHelper;
 import ro.isdc.auth.repository.MovieRepository;
+import ro.isdc.auth.support.ReadOperationParams;
+import ro.isdc.auth.support.ReadOperationResults;
 import ro.isdc.auth.support.UserContextUtil;
 
 @Service
@@ -48,6 +59,37 @@ public class MovieService extends AbstractCrudService<Movie> {
 			return repository.save(movieToSave);
 		}
 
+	}
+
+	@Override
+	public ReadOperationResults read(ReadOperationParams params) {
+
+		String uId = userContext.getUserId();
+
+		ReadOperationResults result = new ReadOperationResults();
+		Direction sortDir = params.getsSortDir_0().equals("asc") ? Direction.ASC : Direction.DESC;
+		String sortColName = params.getsColumns().split(",")[params.getiSortCol_0()];
+		Sort sort = new Sort(sortDir, sortColName);
+		int pageNumber = (int) Math.ceil(params.getiDisplayStart() / params.getiDisplayLength());
+		Page<Movie> page = repository.findByUserId(uId, new PageRequest(pageNumber, params.getiDisplayLength(), sort));
+		List<Movie> data = page.getContent();
+		List<Movie> uiDate = new ArrayList<Movie>();
+		for (Movie entity : data) {
+			uiDate.add(getHelper().copyFrom(entity));
+		}
+		result.setsEcho(params.getsEcho());
+		result.setiTotalDisplayRecords(page.getTotalElements());
+		result.setiTotalRecords(page.getTotalElements());
+		result.setAaData(uiDate);
+		return result;
+
+	}
+
+	@Override
+	public List<Movie> readAll() {
+		String uId = userContext.getUserId();
+
+		return Lists.newArrayList(repository.findByUserId(uId));
 	}
 	// TODO: Override more methods as I see fit.
 
